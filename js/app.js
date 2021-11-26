7d2b619ac0146da3a3f6ff11924ee8cbfac61ace
yaninaslab <yana.belash@gmail.com>
2021-11-26 14:33:07 -0500

    Added comments to my code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-
+// Function to be called when the Weather Forecast button is clicked
 function call_api() {
     document.body.style.backgroundColor = "lightblue";
-
+// URL was taken from the API with the selected params
     axios.request({
        url: "https://api.open-meteo.com/v1/forecast?latitude=43.64&longitude=-79.53&hourly=temperature_2m,precipitation,windspeed_10m",
         
@@ -9,7 +9,7 @@ function call_api() {
 
 }
 
-
+// Function that makes a successful output from the API
 function weather_success(response) {
     var weather_section = document.getElementById("weather_section");
     var weather_message = document.createElement("h1");
@@ -19,7 +19,7 @@ function weather_success(response) {
 
 
 }
-
+// Function that is called when something goes wrong
 function weather_failure(error) {
     var weather_section = document.getElementById("weather_section");
     var error_message = document.createElement("h1");
@@ -27,7 +27,7 @@ function weather_failure(error) {
     weather_section.appendChild(error_message);
 
 }
-
+// Defining a variable and grabbing its id from the page so that we can add events to call a function when the button is clicked
 var weather_forecast = document.getElementById("forecast");
 weather_forecast.addEventListener('click', call_api);
 
